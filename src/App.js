8a173e0bac20f90ea3b599a9d0d09a1874d99ed5
remiyeku <antakin4@gmail.com>
2023-01-31 20:25:09 +0000

    major components for calculator built

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import Wrapper from "../src/components/Wrapper";
+import Screen from "../src/components/Screen";
+import ButtonBox from "../src/components/Buttonbox";
+import Button from "../src/components/Button";
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
+const App = () => {
+  return (
+    <Wrapper>
+      <Screen value="0" />
+      <ButtonBox>
+        {btnValues.flat().map((btn, i) => {
+          return (
+            <Button
+              key={i}
+              className={btn === "=" ? "equals" : ""}
+              value={btn}
+              onClick={() => {
+                console.log(`${btn} clicked!`);
+              }}
+            />
+          );
+        })}
+      </ButtonBox>
+    </Wrapper>
+  );
+};
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import "../css/Button.css";
+
+const Button = ({ className, value, onClick }) => {
+  return (
+    <button className={className} onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Button;
